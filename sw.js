73c21357e6849958acd5a5c9d334c81b54bf9af4
vaiulian <vasileaiulian@gmail.com>
2019-05-21 14:41:06 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,34 @@
+const CACHE_STATIC_NAME = 'static';
+const URLS_TO_PRECACHE = [
+    '/',
+    'index.html',
+    'src/js/app.js',
+    'src/js/feed.js',
+    'src/lib/material.min.js',
+    'src/lib/material.indigo-deep_orange.min.css',
+    'src/css/app.css',
+    'src/css/feed.css',
+    'src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    // 'https://code.getmdl.io/1.3.0/material.indigo-deep_orange.min.css"'
+];
+
 self.addEventListener('install', event => {
     console.log('[Service Worker] Installing Service Worker ...', event);
-    event.waitUntil(self.skipWaiting());
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(cache => {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll(URLS_TO_PRECACHE);
+            })
+            .then(() => {
+                console.log('[ServiceWorker] Skip waiting on install');
+                return self.skipWaiting();
+            })
+    );
 });
+
 
 self.addEventListener('activate', event => {
     console.log('[Service Worker] Activating Service Worker ...', event);
